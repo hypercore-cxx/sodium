702a0300179a93663598357e3499b1ea8e292fbc
heapwolf <paolo@async.ly>
2019-06-12 16:06:04 +0200

    still broken, wip

--- test/index.cxx
+++ test/index.cxx
@@ -35,13 +35,31 @@ int main() {
 
   t.test("generic hash", [](auto t) {
     Hyper::Util::Buffer<uint8_t> in("Hello, world!");
-    Hyper::Util::Buffer<uint8_t> out(crypto_generichash_BYTES_MAX);
 
-    Hyper::Sodium::genericHash(out, in);
+    {
+      Hyper::Util::Buffer<uint8_t> out(crypto_generichash_BYTES);
+      Hyper::Sodium::genericHash(out, in);
 
-    std::string expected = "511bc81dde11180838c562c82bb35f3223f46061ebde4a955c27b3f489cf1e03";
+      std::string expected = "511bc81dde11180838c562c82bb35f823f46061ebde4a955c27b3f489cf1e03";
+      t->equal(out.toString("hex"), expected, "hashed buffer");
+    }
 
-    t->equal(out.toString("hex"), expected, "hashed buffer");
+    {
+      Hyper::Util::Buffer<uint8_t> out(crypto_generichash_BYTES_MIN);
+      Hyper::Sodium::genericHash(out, in);
+
+      std::string expected = "3895c59e4aeb0903396b5be3fbec69fe";
+      t->equal(out.toString("hex"), expected, "hashed buffer min");
+    }
+
+    {
+      Hyper::Util::Buffer<uint8_t> out(crypto_generichash_BYTES_MAX);
+      Hyper::Sodium::genericHash(out, in);
+
+      std::string expected = "7dfdb888af71eae0e6a6b751e8e3413d767ef4fa52a7993daa9ef097f7aa3d949199c113caa37c94f80cf3b22f7d9d6e4f5def4ff927830cffe4857c34be3d89";
+      t->equal(out.toString("hex"), expected, "hashed buffer max");
+    }
+
     t->end();
   });
 }
